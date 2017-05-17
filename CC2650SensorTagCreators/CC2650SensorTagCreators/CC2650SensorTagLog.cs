cbbf0bbda7174ca1a26d4b9a2dfd39ddfcb3824d
DJonesEmbedded <davidjones@sportronics.com.au>
2017-05-18 00:21:26 +1000

    Further refactoring: Moved all BT code out of UI. In process of moving BT code to seperate project as a class library.

---
+++ CC2650SensorTagCreators/CC2650SensorTagCreators/CC2650SensorTagLog.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace CC2650SenorTagCreators
+{
+    /// <summary>
+    /// NB: Must be a top level class.
+    /// </summary>
+    public static class Logging
+    {
+        public static bool KeepCounting = false;
+        public static string LogMsg = "";
+
+        static long PeriodCounter = 0;
+        static long LastEventCount = 0;
+        static Timer EventTimer = null;
+        public static long EventCount;
+
+        static int UpdatePeriod = 15000; //15s
+
+        static private async void EventTimerCallback(object state)
+        {
+            PeriodCounter++;
+            long currentCount;
+            long diff;
+            if (KeepCounting)
+            {
+                currentCount = System.Threading.Interlocked.Read(ref EventCount);
+                diff = currentCount - LastEventCount;
+                LastEventCount = currentCount;
+            }
+            else
+            {
+                diff = 0;
+                diff = System.Threading.Interlocked.Exchange(ref EventCount, diff);
+            }
+
+            Debug.WriteLine(PeriodCounter);
+            string logMsg = sensorCntr.ToString() + "-" + PeriodCounter.ToString() + "-" + diff.ToString();
+            await MainPage.PrependTextStatic(logMsg);
+            LogMsg += logMsg + "\r\n"; ;
+
+
+            if ((PeriodCounter % SensorPeriod) == (SensorPeriod - 1))
+            {
+                PeriodCounter = 0;
+
+                StorageFolder storageFolder = KnownFolders.DocumentsLibrary;
+                var sampleFile = await storageFolder.GetFileAsync("sensors.log");
+                await Windows.Storage.FileIO.AppendTextAsync(sampleFile, LogMsg);
+
+
+                LogMsg = "";
+
+                await RotateEnableSensors();
+
+            }
+        }
+
+        private const long SensorPeriod = 4; //Switch sensor every 5 minutes.
+        private static byte sensorCntr = 0;
+
+        public static bool GetBit(this byte b, int bitNumber)
+        {
+            System.Collections.BitArray ba = new BitArray(new byte[] { b });
+            return ba.Get(bitNumber);
+        }
+
+        private static Dictionary<CC2650SensorTag.SensorServicesCls.SensorIndexes, bool> SensorIsOn;
+
+
+        private static async Task RotateEnableSensors()
+        {
+            StopLogging();
+            sensorCntr++;
+            //Skip any cntr which would have IO on.
+            while (GetBit(sensorCntr, (int)CC2650SensorTag.SensorServicesCls.SensorIndexes.IO_SENSOR))
+                sensorCntr++;
+            string maxSensor = "";
+            for (CC2650SensorTag.SensorServicesCls.SensorIndexes sensor = CC2650SensorTag.SensorServicesCls.SensorIndexes.IR_SENSOR; sensor < (CC2650SensorTag.SensorServicesCls.SensorIndexes.REGISTERS); sensor++)
+            {
+                if (sensor == CC2650SensorTag.SensorServicesCls.SensorIndexes.IO_SENSOR)
+                    continue;
+                //Skip IO
+
+                if (!SensorIsOn.Keys.Contains(sensor))
+                    SensorIsOn.Add(sensor, false);
+
+
+                //else if (sensor == SensorUUIDs.SensorIndexes.KEYS)
+                //    sen++;
+                bool isToBeOn = GetBit(sensorCntr, (int)sensor); //Yes sensor here IS correct as its the sequential counter
+                if (isToBeOn != SensorIsOn[sensor])
+                {
+                    if (SensorIsOn[sensor])
+                    {
+                        if (CC2650SensorTag.TagSensorServices.Sensors.Keys.Contains(sensor))
+                            if (CC2650SensorTag.TagSensorServices.Sensors[sensor] != null)
+                                await CC2650SensorTag.TagSensorServices.TurnOffSensor(CC2650SensorTag.TagSensorServices.Sensors[sensor]);
+                    }
+                    else
+                    {
+                        if (CC2650SensorTag.TagSensorServices.Sensors.Keys.Contains(sensor))
+                            if (CC2650SensorTag.TagSensorServices.Sensors[sensor] != null)
+                                await CC2650SensorTag.TagSensorServices.TurnOnSensor(CC2650SensorTag.TagSensorServices.Sensors[sensor]);
+                    }
+
+                    SensorIsOn[sensor] = isToBeOn;
+                }
+                if (SensorIsOn[sensor])
+                    maxSensor = sensor.ToString() + "," + maxSensor;
+                else
+                    maxSensor = "," + maxSensor;
+
+            }
+
+            StorageFolder storageFolder = KnownFolders.DocumentsLibrary; ;
+            var sampleFile = await storageFolder.GetFileAsync("sensors.log");
+
+            string hdr = string.Format("{0}---{1}\r\n", sensorCntr, maxSensor);
+            Debug.WriteLine("{0}-{1}\r\n", sensorCntr, hdr);
+            await Windows.Storage.FileIO.AppendTextAsync(sampleFile, hdr);
+
+            await MainPage.PrependTextStatic(hdr);
+            ContinueLogging();
+
+        }
+
+        public static async Task StartLogging()
+        {
+            await MainPage.PrependTextStatic("cls");
+            LogMsg = "";
+            sensorCntr = 0;
+            SensorIsOn = new Dictionary<CC2650SensorTag.SensorServicesCls.SensorIndexes, bool>();
+            PeriodCounter = 0;
+            LastEventCount = 0;
+
+            StorageFolder storageFolder = KnownFolders.DocumentsLibrary; ;
+            var sampleFile = await storageFolder.CreateFileAsync("sensors.log",
+                    CreationCollisionOption.ReplaceExisting);
+            await RotateEnableSensors();
+
+            System.Threading.Interlocked.Exchange(ref EventCount, 0);
+            ContinueLogging();
+        }
+
+        public static void ContinueLogging()
+        {
+            EventTimer = new Timer(EventTimerCallback, null, 0, (int)UpdatePeriod);
+        }
+
+        public static void StopLogging()
+        {
+            if (EventTimer != null)
+                EventTimer.Dispose();
+        }
+
+    }
+}
+
+
---
+++ CC2650SensorTagCreators/CC2650SensorTagCreators/CC2650SensorTagUnpairedBTConnectivity.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+
+namespace CC2650SenorTagCreators
+{
+    public sealed partial class CC2650SensorTag
+    {
+        public class CC2650SensorTagUnpairedBTConnectivity
+        {
+            BluetoothLEAdvertisementWatcher BLEAdvWatcher;
+
+            public CC2650SensorTag.TagSensorServices TagServices { get; internal set; } = null;
+
+            public CC2650SensorTagUnpairedBTConnectivity()
+            {
+                TagServices = new CC2650SensorTag.TagSensorServices();
+            }
+
+            long barrier = 0;
+
+            public void Start()
+            {
+                BluetoothLEAdvertisementFilter blaf = new BluetoothLEAdvertisementFilter();
+
+
+                BLEAdvWatcher = new BluetoothLEAdvertisementWatcher();
+                BLEAdvWatcher.Received += Bleaw_Received;
+                System.Threading.Interlocked.Exchange(ref barrier, 0);
+
+                BLEAdvWatcher.Start();
+            }
+
+            private async void Bleaw_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+            {
+                if (sender == null)
+                    return;
+                bool OK = false;
+                if (System.Threading.Interlocked.Increment(ref barrier) == 1)
+                {
+                    BLEAdvWatcher.Stop();
+                    Guid guidNotification;
+                    ulong blAdress = args.BluetoothAddress; ;
+                    BluetoothLEDevice blDevice = await
+                        Windows.Devices.Bluetooth.BluetoothLEDevice.FromBluetoothAddressAsync(blAdress);
+                    if (!(blDevice == null))
+                    {
+                        var name = blDevice.Name;
+                        if (blDevice.DeviceInformation.Kind ==
+                                Windows.Devices.Enumeration.DeviceInformationKind.AssociationEndpoint)
+                        {
+
+                            var scanresp = args.AdvertisementType;
+                            Windows.Devices.Enumeration.DeviceAccessStatus result;
+                            try
+                            {
+                                result = await blDevice.RequestAccessAsync();
+                            }
+                            catch (Exception ex)
+                            {
+                                result = Windows.Devices.Enumeration.DeviceAccessStatus.DeniedBySystem;
+                            }
+                            if (result == Windows.Devices.Enumeration.DeviceAccessStatus.Allowed)
+                            {
+
+                                name = blDevice.Name;
+                                System.Diagnostics.Debug.WriteLine("Endpoint Device Name: {0}", name);
+                                await MainPage.PrependTextStatic(string.Format("Endpoint Device Name: {0}", name));
+                                var services = await blDevice.GetGattServicesAsync();
+                                var svcs = services.Services;
+                                System.Diagnostics.Debug.WriteLine("Endpoint Device Id: {0}", blDevice.DeviceId);
+                                await MainPage.PrependTextStatic(string.Format("Endpoint Device Id: {0}", blDevice.DeviceId));
+                                System.Diagnostics.Debug.WriteLine("Start");
+
+
+                                await TagServices.InterogateServices(svcs);
+                                OK = true;
+                            }
+                        }
+                    }
+                }
+                if (!OK)
+                {
+                    System.Threading.Interlocked.Decrement(ref barrier);
+                    BLEAdvWatcher.Start();
+                }
+            }
+
+            internal void Stop()
+            {
+                if (BLEAdvWatcher != null)
+                    if (BLEAdvWatcher.Status == BluetoothLEAdvertisementWatcherStatus.Started)
+                        BLEAdvWatcher.Stop();
+            }
+        }
+    }
+}
